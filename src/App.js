e1fa1f810f7abd316a09ec238c9f5ca16712759c
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-11-14 21:58:55 -0300

    mobile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import image from "./images/illustration-sign-up-mobile.svg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="img">
+        <img src={image} className="image" alt="logo" />
+      </div>
+
+      <div className="title">
+        <p>Stay updated!</p>
+      </div>
+
+      <div className="subtitle">
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+      </div>
+
+      <div className="list">
+        <li>Product discovery and building what matters</li>
+        <li>Measuring to ensure updates are a success</li>
+        <li>And much more!</li>
+      </div>
+
+      <div className="email">
+        <div className="email_title">
+          <p>Email address</p>
+        </div>
+
+        <div className="email_input">
+          <input placeholder="email@company.com"></input>
+        </div>
+
+        <div className="email_btn">
+          <button>Subscribe to monthly newsletter</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
